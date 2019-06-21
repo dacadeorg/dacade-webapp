b5c94d6854cef2f0b239e138e18dada7530d1d47
moritzfelipe <stellmacher@gmail.com>
2019-06-21 10:54:44 +0200

    Removed vuex for content display, just need to read once

--- pages/community/_id.vue
+++ pages/community/_id.vue
@@ -9,27 +9,20 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-import { mapGetters, mapActions } from 'vuex'
+import firebase from '@/plugins/firebase'
 import Navigation from '@/components/Navigation'
+
+const db = firebase.database()
 
 export default {
   components: {
     Navigation
   },
-
-  computed: {
-    ...mapGetters({
-      communityData: 'content/communityData'
-    })
-  },
-
-  created() {
-    this.contentInit()
-  },
-
-  methods: {
-    ...mapActions({
-      contentInit: 'content/init'
+  asyncData({ params }) {
+    return db.ref(`communityData/${params.id}`).once('value').then((snapShot) => {
+      let communityData = snapShot.val()
+      communityData.key = params.id // snapShot.key
+      return { communityData }
     })
   }
 }
