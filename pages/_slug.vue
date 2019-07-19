f862b8abefb6293a3a1577cfe2d5319a83b0438f
moritzfelipe <stellmacher@gmail.com>
2019-07-19 10:52:16 +0200

    Cleaned up _slug view

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -3,37 +3,20 @@
     <Navigation />
     <CommunityNavigation />
     <div class="container">
-      <nuxt-child :community-data="communityData" />
+      <nuxt-child/>
     </div>
   </div>
 </template>
 <script>
+/* eslint-disable no-console */
 import firebase from '@/plugins/firebase'
 import Navigation from '@/components/Navigation'
 import CommunityNavigation from '@/components/CommunityNavigation'
-import { mapGetters } from 'vuex'
-/* eslint-disable no-console */
 
 export default {
   components: {
     Navigation,
     CommunityNavigation
-  },
-  data() {
-    return {
-    }
-  },
-  computed: {
-    ...mapGetters({
-      user: 'user'
-    })
-  },
-  asyncData({ params }) {
-    return firebase.database().ref(`LCData/${params.slug}`).once('value').then((snapShot) => {
-      const communityData = snapShot.val()
-      communityData.key = params.slug // snapShot.key
-      return { communityData }
-    })
   },
   fetch({ store, params }) {
     return firebase.database().ref(`LCData/${params.slug}`).once('value').then((snapShot) => {
