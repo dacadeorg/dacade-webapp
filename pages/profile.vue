535d7a1be8ece2b908de7c9ce94a2ca4a8ea2c88
moritzfelipe <stellmacher@gmail.com>
2019-08-15 12:44:09 +0200

    Show communities in profile view

--- pages/profile.vue
+++ pages/profile.vue
@@ -7,7 +7,14 @@
           <div v-if="user">
             <h2 class="h-dark">
               {{ user.displayName }}
+            <span>
+              <img src="/img/ITB-Cover.png" style="object-fit:cover; width:40px; height:40px; border-radius:250px;" alt="ITB">
+              <img src="/img/WD1_cover.png" style="object-fit:cover; width:40px; height:40px; border-radius:250px; margin-left:-0.75em;" alt="ITB">
+              <img src="/img/AE-DEV-101-Cover.png" style="object-fit:cover; width:40px; height:40px; border-radius:250px; margin-left:-0.75em;" alt="ITB">
+              <img src="/img/ED1_cover.png" style="object-fit:cover; width:40px; height:40px; border-radius:250px;  margin-left:-0.75em;" alt="ITB">
+            </span>
             </h2>
+
             <h4 class="muted-dark">
               {{ user.email }}
             </h4>
@@ -53,6 +60,87 @@
               </h5>
               <div class="btn-edit btn mt-2 mb-6">
                 Edit
+              </div>
+            </div>
+            <div>
+              <h2 class="mb-4">Communities</h2>
+              <div class="float-left mr-3">
+                <img src="/img/ITB-Cover.png" style="object-fit:cover; width:100px; height:100px; border-radius:250px;" alt="ITB">
+              </div>
+              <div class="mb-4">
+                <h3 class="learning-color">
+                  Introduction to Blockchain
+                </h3>
+                <h5>Learning Points:
+                  <span class="learning-color">
+                    14/20
+                  </span>
+                </h5>
+                <h5>Teaching Points:
+                  <span class="teaching-color">
+                    2
+                  </span>
+                </h5>
+                <div class="btn btn-cash-out mt-2" style="margin-left:7em; color:#53D1AF; border:1.6px solid #53D1AF">
+                  See certificate
+                </div>
+              </div>
+              <div class="float-left mr-3">
+                <img src="/img/AE-DEV-101-Cover.png" style="object-fit:cover; width:100px; height:100px; border-radius:250px;" alt="ITB">
+              </div>
+              <div class="mb-4">
+                <h3 style="color:#f22f70">Aeternity Development 101</h3>
+                <h5>Learning Points:
+                  <span class="learning-color">
+                    14/20
+                  </span>
+                </h5>
+                <h5>Teaching Points:
+                  <span class="teaching-color">
+                    2
+                  </span>
+                </h5>
+                <div class="btn btn-cash-out mt-2" style="margin-left:7em; color:#f22f70; border:1.6px solid #f22f70">
+                  See certificate
+                </div>
+              </div>
+              <div class="float-left mr-3">
+                <img src="/img/WD1_cover.png" style="object-fit:cover; width:100px; height:100px; border-radius:250px;" alt="ITB">
+              </div>
+              <div class="mb-4">
+                <h3 style="color:#2170f3">Web Development 101</h3>
+                <h5>Learning Points:
+                  <span class="learning-color">
+                    14/20
+                  </span>
+                </h5>
+                <h5>Teaching Points:
+                  <span class="teaching-color">
+                    2
+                  </span>
+                </h5>
+                <div class="btn btn-cash-out mt-2" style="margin-left:7em; color:#2170f3; border:1.6px solid #2170f3">
+                  See certificate
+                </div>
+              </div>
+              <div class="float-left mr-3">
+                <img src="/img/ED1_cover.png" style="object-fit:cover; width:100px; height:100px; border-radius:250px;" alt="ITB">
+              </div>
+              <div class="mb-4">
+                <h3 style="color:#a1a1a1">Ethereum Development 101</h3>
+                <h5>Learning Points:
+                  <span class="learning-color">
+                    14/20
+                  </span>
+                </h5>
+                <h5>Teaching Points:
+                  <span class="teaching-color">
+                    2
+                  </span>
+                </h5>
+                <div class="btn btn-cash-out mt-2" style="margin-left:7em; color:#a1a1a1; border:1.6px solid #a1a1a1">
+                  See certificate
+                </div>
               </div>
             </div>
           </div>
