921bda51f48197a884d21629d0a5ee0a76b53ea6
lewisDylan7 <lewisdylan@octangrp.com>
2020-06-18 13:17:43 +0200

    Create new auth file for authenthication functions
    Create the new file to move all the auth functions from index
    to the auth file

---
+++ store/auth.js
@@ -0,0 +1,168 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable spaced-comment */
+import firebase from '@/plugins/firebase'
+import { vuexfireMutations, firebaseAction } from 'vuexfire'
+const db = firebase.database()
+
+export const actions = {
+  signUpUser({ commit }, payload) {
+    commit('setBusy', true)
+    commit('clearError')
+    let newUser = null
+    let user = null
+    firebase.database().ref('users').orderByChild('displayName').equalTo(`${payload.name}`).once('value')
+      .then((snapShot) => {
+        if (snapShot.exists()) {
+          const error = { message: 'This username is already in use' }
+          commit('setBusy', false)
+          commit('setError', error)
+        } else {
+          firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+            .then(() => {
+              user = firebase.auth().currentUser
+              newUser = user
+              return user.updateProfile({ displayName: payload.name })
+                .then(() => {
+                  const currentUser = {
+                    id: user.uid,
+                    email: payload.email,
+                    displayName: payload.name
+                  }
+                  commit('setUser', currentUser)
+                })
+            })
+            .then(() => {
+              const userData = {
+                displayName: payload.name
+              }
+              return firebase.database().ref(`users/${user.uid}`).set(userData)
+            })
+            .then(() => {
+              const notification = {
+                date: Date.now(),
+                displayName: payload.name,
+                link: '/communities',
+                message: `👋 Welcome to dacade ${payload.name}! In this notification view, you will be notified about your successes, like earnings from your submissions and reviews. To get started, click on this link and choose a learning community.`,
+                notificationRead: false,
+                userId: user.uid
+              }
+              return firebase.database().ref(`notifications/${user.uid}`).push(notification)
+            }).then(() => {
+              this.$ga.event({
+                eventCategory: 'registration',
+                eventAction: `registration userId: ${user.uid}`
+              })
+            })
+            .then(() => {
+              commit('setJobDone', true)
+              commit('setBusy', false)
+            })
+            .catch((error) => {
+              commit('setBusy', false)
+              commit('setError', error)
+            })
+        }
+      })
+  },
+  loginUser({ commit }, payload) {
+    commit('setBusy', true)
+    commit('clearError')
+    firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+      .then(() => {
+        commit('setJobDone', true)
+        commit('setBusy', false)
+      })
+      .catch((error) => {
+        commit('setBusy', false)
+        commit('setError', error)
+      })
+  },
+  logOut({ commit }) {
+    firebase.auth().signOut()
+    commit('setUser', null)
+  },
+  passwordResetRequest({ commit }, payload) {
+    commit('setBusy', true)
+    commit('clearError')
+    firebase.auth().sendPasswordResetEmail(payload.email)
+      .then(() => {
+        commit('setJobDone', true)
+        commit('setBusy', false)
+      })
+      .catch((error) => {
+        commit('setBusy', false)
+        commit('setError', error)
+      })
+  },
+  getUserNotifications: firebaseAction(({ bindFirebaseRef }, uid) => {
+    bindFirebaseRef('userNotifications', db.ref('notifications').child(uid))
+  }),
+  getUserLearningPoints: firebaseAction(({ bindFirebaseRef }, uid) => {
+    bindFirebaseRef('userLearningPoints', db.ref('learningPoints').child(uid))
+  }),
+  getUserBalance: firebaseAction(({ bindFirebaseRef }, uid) => {
+    bindFirebaseRef('userBalance', db.ref('balance').child(uid))
+  }),
+  getUserReputation: firebaseAction(({ bindFirebaseRef }, uid) => {
+    bindFirebaseRef('userReputation', db.ref('reputation').child(uid))
+  }),
+  setUserNotificationSeen({ commit }, payload) {
+    db.ref(`notifications/${payload.userId}/${payload.id}/notificationRead`).set(true)
+      .then(() => {
+        commit('setJobDone', true)
+        commit('setBusy', false)
+      })
+      .catch((error) => {
+        commit('setBusy', false)
+        commit('setError', error)
+      })
+  },
+  addUserNotification({ commit }, payload) {
+    db.ref(`notifications/${payload.userId}`).push(payload)
+      .then(() => {
+        console.log('success user notification added')
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
+  },
+  updateWalletAddress({ commit }, payload) {
+    console.log(payload)
+    db.ref(`userWallet/${payload.userId}/${payload.token}`).set(`${payload.walletAddress}`)
+      .then(() => {
+        commit('setJobDone', true)
+        commit('setBusy', false)
+      })
+      .catch((error) => {
+        commit('setBusy', false)
+        commit('setError', error)
+      })
+  },
+  createVerificationRequest({ commit }, payload) {
+    db.ref(`userVerificationRequest/${payload.userId}/${payload.verificationType}`).set({
+      displayName: payload.displayName,
+      link: payload.verificationLink
+    })
+      .then(() => {
+        commit('setJobDone', true)
+        commit('setBusy', false)
+      })
+      .catch((error) => {
+        commit('setBusy', false)
+        commit('setError', error)
+      })
+  }
+}
+
+export const mutations = {
+  setUser(state, payload) {
+    state.user = payload
+  },
+  ...vuexfireMutations
+}
+export const getters = {
+  user(state) {
+    return state.user
+  }
+}
