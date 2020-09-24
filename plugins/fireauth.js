b054ab3bbeec2c0b26166b19edb1938100eecd9d
Yannick <musayann@octangrp.com>
2020-09-24 18:03:27 +0200

    Fetch admin data for admin routes only

--- plugins/fireauth.js
+++ plugins/fireauth.js
@@ -1,8 +1,7 @@
 import firebase from '@/plugins/firebase'
 
 export default (context) => {
-  const { store } = context
-
+  const { store, route } = context
   return new Promise((resolve, reject) => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -11,13 +10,19 @@ export default (context) => {
           email: user.email,
           displayName: user.displayName
         })
-        store.dispatch('admin/fetch', {
-          id: user.uid
-        }).then(() => {
+        // fetch the admin data for admin routes
+        if (route.path.startsWith('/admin')) {
+          store.dispatch('admin/fetch', {
+            id: user.uid
+          }).then(() => {
+            return resolve(user)
+          }).catch(() => {
+            return resolve(user)
+          })
+        } else {
+          // if routes are not for admin just return the user
           return resolve(user)
-        }).catch(() => {
-          return resolve(user)
-        })
+        }
       } else {
         store.commit('user/set', null)
         return resolve(user)
