e4e5da1e644d6b850f30cb0c8d1b270fe24c7003
musayann <musayann@octangrp.com>
2021-03-22 12:51:00 +0200

    Fix start link

---
+++ pages/communities/_slug/challenge.vue
@@ -0,0 +1,44 @@
+<template>
+  <Section padding="py-0">
+    <div class="flex">
+      <div class="w-1/4 py-14">
+        <Navigation />
+      </div>
+      <div class="w-3/4">
+        <div
+          class="py-4 flex flex-col divide-y divide-solid divide-gray-200 space-y-8 text-gray-700"
+        >
+          <PageNavigation />
+        </div>
+      </div>
+    </div>
+  </Section>
+</template>
+<script>
+/* eslint-disable no-console */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+import { mapGetters } from 'vuex'
+import Navigation from '@/components/sections/communities/Navigation'
+import PageNavigation from '@/components/sections/communities/PageNavigation.vue'
+import Section from '@/components/ui/Section'
+
+export default {
+  components: {
+    Navigation,
+    Section,
+    PageNavigation
+  },
+  scrollToTop: true,
+  async fetch ({ store, params }) {
+    await store.dispatch('communities/find', params.slug)
+    await store.dispatch('communities/content', params.slug)
+  },
+  computed: {
+    ...mapGetters({
+      communityData: 'content/communityData',
+      community: 'communities/current'
+    })
+  }
+}
+</script>
