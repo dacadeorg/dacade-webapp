85fc7218d676c5c0d8451c16865c3e475c7b857d
karemarsy <karemarsy@gmail.com>
2021-04-21 13:28:18 +0200

    Fix the font issue and ratio aspect

--- components/cards/Wallet.vue
+++ components/cards/Wallet.vue
@@ -4,7 +4,7 @@
   >
     <div class="bg-gray-50 lg:w-60 md:w-60 sm:w-60 rounded-3.5xl">
       <div class="p-6 ">
-        <div class="border-b-2 border-dotted border-black">
+        <div class="border-b border-dotted border-gray-900">
           <h1 class="text-2xl">
             {{ title }}
           </h1>
@@ -14,30 +14,30 @@
           </div>
         </div>
         <div class="flex">
-          <div class="w-1/2 pt-5 ">
+          <div class="w-1/2 pt-5 text-sm">
             <h1>Balance</h1>
           </div>
-          <div class="w-1/2 pt-3.5 text-right text-2xl font-bold">
+          <div class="w-1/2 pt-3.5 text-right text-2xl font-medium">
             <h1>{{ amount }}</h1>
           </div>
         </div>
       </div>
     </div>
-    <div class="px-7 pt-6 lg:w-8/12 md:w-8/12 sm:w-8/12 pb-24 lg:pb-24">
-      <div class="xl:w-72 md:w-72 lg:w-72 text-sm text-gray-700">
+    <div class="px-7 pt-6 lg:w-6/12 md:w-8/12 sm:w-8/12 pb-24 lg:pb-24">
+      <div class="xl:w-72 md:w-72 lg:w-full text-sm text-gray-700">
         <p>
           Laborum soluta distinctio laborum laudantium velit asperiores autem
           veniam.
         </p>
       </div>
       <div
-        class="lg:pt-5 sm:pt-5 md:pt-5 pt-14 absolute text-sm lg:text-gray-700 md:text-gray-700 sm:text-gray-700 cursor-pointer text-primary font-bold lg:font-normal md:font-normal sm:font-normal"
+        class="lg:pt-5 sm:pt-5 md:pt-5 pt-14 absolute text-sm lg:text-gray-700 md:text-gray-700 sm:text-gray-700 cursor-pointer text-primary font-medium lg:font-normal md:font-normal sm:font-normal"
       >
         <a href="#">Change Address</a>
       </div>
       <div class="right-2 absolute bottom-2 mt-5">
         <Button :padding="false" class="py-2" type="outline-primary">
-          <span class="inline-block ">Cash out </span>
+          <span class="inline-block text-sm">Cash out </span>
           <span class="inline-block lg:pl-12 pl-3"><ArrowRight /></span>
         </Button>
       </div>
