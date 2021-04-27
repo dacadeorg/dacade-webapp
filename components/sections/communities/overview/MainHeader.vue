adbb37e361f6dc2b13a99e538f0574ade50b0885
Agusti Fernandez Pardo <me@agusti.me>
2021-04-27 05:51:57 +0000

    Communities > MainHeader: Add avatar faces, make responsive, add themeWrapper

--- components/sections/communities/overview/MainHeader.vue
+++ components/sections/communities/overview/MainHeader.vue
@@ -1,38 +1,38 @@
 <template>
-  <Section :custom-color="community.colors.primary" padding="py-24">
-    <div
-      class="relative flex"
-      :style="{
-        color: community.colors.text
-      }"
-    >
-      <div class="w-6/12 flex flex-col justify-between">
-        <div class="relative">
-          <h1 class="text-7.75xl leading-none mb-5 font-normal">
+  <Section class="bg-theme-primary text-theme-text">
+    <div class="max-w-3xl px-5 py-8 mx-auto lg:max-w-7xl sm:py-10 sm:px-10">
+      <div class="flex flex-col justify-between lg:flex-row">
+        <div class="max-w-md mb-4 md:max-w-md lg:max-w-lg xl:max-w-xl lg:mr-6">
+          <h1 class="text-5xl tracking-tight sm:text-6xl xl:text-7xl max-w-text-xs">
             {{ community.name }}
           </h1>
-          <span class="text-.5xl leading-snug">
-            {{ htmlTagsCleanup(communityData.introductionText || community.description || community.summary) }}
-          </span>
+          <p class="mt-2 text-xl leading-tight max-w-text-sm lg:hidden">
+            {{ htmlTagsCleanup(community.summary) }}
+          </p>
+          <p class="hidden mt-4 text-lg xl:text-2xl max-w-text-md lg:block">
+            {{ htmlTagsCleanup(community.description || community.summary) }}
+          </p>
         </div>
-        <div class="relative flex divide-x divide-current leading-loose mt-24">
-          <span
-            class="block py-1 px-3"
-          >{{ community.submissions }}
-            {{ $t('communities.submissions') }}</span>
-          <span
-            class="block py-1 px-3"
-          >{{ communityData.totalFeedback || 0 }} {{ $t('communities.feedbacks') }}</span>
+        <div class="self-end w-1/2 max-w-md">
+          <img :src="community.icon" class="relative w-full aspect-w-1 aspect-h-1">
         </div>
       </div>
-      <div class="w-4/12 text-right ml-auto">
-        <img :src="community.icon" class="w-full">
+      <div class="flex flex-col max-w-xs xl:-mt-2 md:max-w-xl md:flex-row md:items-center">
+        <div class="flex h-10 mr-5 ">
+          <img class="w-10 h-10 -mr-3 border-2 rounded-full border-theme-secondary" src="~assets/img/testimonial-sample-2.png" alt="">
+          <img class="w-10 h-10 -mr-3 border-2 rounded-full border-theme-secondary" src="~assets/img/testimonial-sample-5.png" alt="">
+          <img class="w-10 h-10 -mr-3 border-2 rounded-full border-theme-secondary" src="~assets/img/testimonial-sample-2.png" alt="">
+        </div>
+        <div class="my-2 text-sm">
+          <span><strong>48</strong> Submissions</span>
+          <span class="p-2 md:border-l md:ml-2"><strong>157</strong> Feedbacks</span>
+          <div />
+        </div>
       </div>
     </div>
   </Section>
 </template>
 <script>
-/* eslint-disable no-console */
 import { mapGetters } from 'vuex'
 import Section from '@/components/ui/Section'
 
---
+++ components/wrappers/ThemeWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :style="cssVars">
+    <slot />
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'ThemeWrapper',
+  props: {
+    colors: {
+      default: null,
+      type: Object
+    }
+  },
+  computed: {
+    cssVars () {
+      return {
+        '--tm-primary': this.colors.primary,
+        '--tm-secondary': this.colors.secondary,
+        '--tm-text': this.colors.text,
+        '--tm-highlight': this.colors.highlight,
+        '--tm-accent': this.colors.accent,
+        '--tm-muted': this.colors.muted
+      }
+    }
+  }
+}
+</script>
