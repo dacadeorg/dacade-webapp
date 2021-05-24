921f3c80c979f7a058c22352e375b28448eca49e
Agusti Fernandez Pardo <me@agusti.me>
2021-05-24 14:16:43 +0000

    Make communities navigation and scorebaord use community.colors and themewrapper, fixe body width when user popup open

--- components/popups/User.vue
+++ components/popups/User.vue
@@ -146,6 +146,8 @@ export default {
         const scrollY = body.style.top
         body.style.position = 'relative'
         body.style.top = ''
+        body.style.width = '100%'
+
         window.scrollTo(0, parseInt(scrollY || '0') * -1)
       }
     },
--- components/sections/communities/Navigation.vue
+++ components/sections/communities/Navigation.vue
@@ -1,40 +1,44 @@
 <template>
-  <ul class="relative">
-    <li v-for="(menu, i) in menus" :key="i" class="relative mb-8">
-      <span
-        v-if="!menu.hideTitle"
-        class="relative text-xs font-semibold uppercase"
-        >{{ menu.title }}</span
-      >
-      <ul>
-        <li
-          v-for="(item, k) in menu.items"
-          :key="k"
-          class="relative mt-4 text-sm"
-          :style="activeLinkStyle"
+  <ThemeWrapper :colors="community.colors">
+    <ul class="relative">
+      <li v-for="(menu, i) in menus" :key="i" class="relative mb-8">
+        <span
+          v-if="!menu.hideTitle"
+          class="relative text-xs font-semibold uppercase"
+          >{{ menu.title }}</span
         >
-          <nuxt-link
-            :to="item.link"
-            class="relative text-gray-500"
-            :class="{ 'activable-link': !item.exact }"
+        <ul>
+          <li
+            v-for="(item, k) in menu.items"
+            :key="k"
+            class="relative mt-4 text-sm"
+            :style="activeLinkStyle"
           >
-            <span class="absolute inline-block -left-6 nav-icon">
-              <ChevronRightIcon />
-            </span>
-            <span class="nav-label">{{ item.label }}</span>
-          </nuxt-link>
-        </li>
-      </ul>
-    </li>
-  </ul>
+            <nuxt-link
+              :to="item.link"
+              class="relative text-gray-500"
+              :class="{ 'activable-link': !item.exact }"
+            >
+              <span class="absolute inline-block -left-6 nav-icon">
+                <ChevronRightIcon />
+              </span>
+              <span class="nav-label">{{ item.label }}</span>
+            </nuxt-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </ThemeWrapper>
 </template>
 <script>
 /* eslint-disable no-console */
 import { mapGetters } from 'vuex'
+import ThemeWrapper from '@/components/wrappers/ThemeWrapper'
 import ChevronRightIcon from '~/assets/icons/chevron-right.svg?inline'
 
 export default {
   components: {
+    ThemeWrapper,
     ChevronRightIcon,
   },
   data() {
@@ -42,13 +46,14 @@ export default {
   },
   computed: {
     ...mapGetters({
+      communityData: 'communities/content',
       community: 'communities/current',
       colors: 'ui/colors',
       menus: 'communities/navigation',
     }),
     activeLinkStyle() {
       return {
-        color: this.colors.textAccent,
+        color: this.community.colors.accent,
       }
     },
   },
@@ -79,7 +84,7 @@ export default {
           title: this.$t('communities.navigation.chapters'),
           items: this.community.chapters?.map((chapter, i) => ({
             label: chapter.title,
-            link: this.chapterPath(chapter.id),
+            link: this.chapterPath(i),
             exact: false,
             items: chapter.materials
               ? chapter.materials.map((material) => ({
@@ -92,38 +97,28 @@ export default {
         },
         {
           title: this.$t('communities.navigation.bounties'),
-          items: this.bountyLinks(),
+          items: [
+            {
+              label: this.$t('communities.navigation.challenge'),
+              link: this.communityPath('challenge'),
+              exact: false,
+            },
+            {
+              label: this.$t('communities.navigation.submissions'),
+              link: this.communityPath('submissions'),
+              exact: false,
+            },
+            {
+              label: this.$t('communities.navigation.scoreboard'),
+              link: this.communityPath('scoreboard'),
+              exact: false,
+            },
+          ],
         },
       ]
       this.$store.commit('communities/setNavigation', {
         list,
       })
-    },
-    bountyLinks() {
-      const links = []
-
-      if (this.community.challenges?.length) {
-        links.push({
-          label: this.$t('communities.navigation.challenge'),
-          link: this.communityPath(
-            `challenges/${this.community.challenges[0].id}`
-          ),
-          exact: false,
-        })
-
-        links.push({
-          label: this.$t('communities.navigation.submissions'),
-          link: this.communityPath('submissions'),
-          exact: false,
-        })
-      }
-
-      links.push({
-        label: this.$t('communities.navigation.scoreboard'),
-        link: this.communityPath('scoreboard'),
-        exact: false,
-      })
-      return links
     },
   },
 }
