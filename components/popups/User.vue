406b36ae77a937f6014d467a2b19dce282bcf185
Agusti Fernandez Pardo <me@agusti.me>
2021-05-25 15:02:51 +0200

    fix(user-popup): body width 100% when show true

--- components/popups/User.vue
+++ components/popups/User.vue
@@ -140,13 +140,13 @@ export default {
           document.documentElement.style.getPropertyValue('--scroll-y')
         const body = document.body
         body.style.position = 'fixed'
+        body.style.width = '100%'
         body.style.top = `-${scrollY}`
       } else {
         const body = document.body
         const scrollY = body.style.top
         body.style.position = 'relative'
         body.style.top = ''
-        body.style.width = '100%'
 
         window.scrollTo(0, parseInt(scrollY || '0') * -1)
       }
