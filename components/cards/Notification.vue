3a5cef8e9fc0e2fa796f87ae6624a550a812b6b6
Agusti Fernandez Pardo <me@agusti.me>
2021-06-11 11:32:46 +0200

    feat(notifications): adds link and hover state to notification cards

--- components/cards/Notification.vue
+++ components/cards/Notification.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="flex">
-    <div class="flex mr-2">
-      <Avatar :user="user" class="w-10 h-10" />
+  <nuxt-link :to="details.link">
+    <div class="flex hover:bg-gray-50 -mx-5 px-5 py-4">
+      <div class="flex mr-2">
+        <Avatar :user="user" class="w-10 h-10" />
+      </div>
+      <div class="pt-1 -mt-2">
+        <span class="block text-base text-gray-700">{{ details.message }}</span>
+        <span class="block text-gray-900 font-medium text-sm">{{ date }}</span>
+      </div>
     </div>
-    <div class="pt-1 -mt-2">
-      <span class="block text-base text-gray-700">{{ details.message }}</span>
-      <span class="block text-gray-900 font-medium text-sm">{{ date }}</span>
-    </div>
-  </div>
+  </nuxt-link>
 </template>
 
 <script>
@@ -41,6 +43,10 @@ export default {
     date() {
       return moment(this.details.created_at).fromNow()
     },
+  },
+  mounted() {
+    // eslint-disable-next-line no-console
+    console.log(this.details)
   },
 }
 </script>
