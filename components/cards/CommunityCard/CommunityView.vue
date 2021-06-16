cfd6b77c69ff1c28489ae5fd4a18645c559faa15
Agusti Fernandez Pardo <me@agusti.me>
2021-06-16 17:07:42 +0200

    feat(community-card): make hover state

--- components/cards/CommunityCard/CommunityView.vue
+++ components/cards/CommunityCard/CommunityView.vue
@@ -2,7 +2,16 @@
   <ThemeWrapper :colors="community.colors">
     <nuxt-link :to="{ path: path }">
       <div
-        class="w-full bg-gray-200 lg:flex min-w-full mx-0 rounded-3xl relative"
+        class="
+          group
+          w-full
+          bg-gray-200
+          lg:flex
+          min-w-full
+          mx-0
+          rounded-3xl
+          relative
+        "
       >
         <CommunityVisual :community="community" />
         <div class="flex-col justify-between flex p-3 md:p-7 text-gray-700">
@@ -12,17 +21,49 @@
             </div>
           </div>
           <div class="divide-y divide-dotted divide-gray-500 space-y-5">
-            <div class="text-base text-right flex-end">
-              <span class="font-light">
-                {{ $t('communities.card.estimated') }}
-              </span>
-              <span class="font-medium">
-                {{ duration }} {{ $t('communities.card.minutes') }}
-              </span>
-            </div>
             <div class="flex space-x-3">
               <div v-for="(reward, i) in community.rewards" :key="i">
                 <Reward :reward="reward" />
+              </div>
+            </div>
+            <div class="hidden md:flex flex-row justify-between">
+              <div class="text-base pt-7 text-left flex-start flex flex-col">
+                <span class="font-light">
+                  {{ $t('communities.card.estimated') }}
+                </span>
+                <span class="font-medium">
+                  {{ duration }} {{ $t('communities.card.minutes') }}
+                </span>
+              </div>
+              <div class="mt-7 align-middle">
+                <nuxt-link :to="path">
+                  <Button
+                    class="
+                      group-hover:bg-primary
+                      bg-gray-200
+                      border
+                      text-primary
+                      group-hover:text-white
+                      border-primary
+                    "
+                  >
+                    <span
+                      class="
+                        text-primary
+                        group-gover:bg-text-primary group-hover:text-white
+                        py-0
+                        inline-block
+                        align-middle
+                      "
+                      >{{ $t('page.index.main.button') }}</span
+                    >
+                    <span class="inline-block align-middle">
+                      <ArrowRightIcon
+                        class="ml-7 text-primary group-hover:text-white"
+                      />
+                    </span>
+                  </Button>
+                </nuxt-link>
               </div>
             </div>
           </div>
@@ -37,10 +78,12 @@ import Reward from '@/components/ui/Reward'
 import CommunityVisual from '@/components/cards/CommunityCard/CommunityVisual'
 import ThemeWrapper from '@/components/wrappers/ThemeWrapper'
 import moment from 'moment'
+import Button from '@/components/ui/Button'
+import ArrowRightIcon from '~/assets/icons/arrow-right.svg?inline'
 
 export default {
   name: 'CommunityCardView',
-  components: { Reward, CommunityVisual, ThemeWrapper },
+  components: { Reward, CommunityVisual, ThemeWrapper, Button, ArrowRightIcon },
   props: {
     community: {
       default: () => {},
