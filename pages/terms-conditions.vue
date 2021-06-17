146033986e914ed3f7f0a7de9134ff0e0008bb60
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2021-06-17 16:04:32 +0000

    V2 terms

---
+++ pages/terms-conditions.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content-wrapper">
+    <article class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+      <nuxt-content :document="terms" />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const terms = await $content('legal/terms').fetch()
+    return {
+      terms,
+    }
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+}
+</script>
