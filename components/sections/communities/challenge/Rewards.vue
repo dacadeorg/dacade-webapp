c07192995aa3589b2287f216647b5ad5ff35ff62
musayann <musayann@octangrp.com>
2021-06-17 14:26:46 +0200

    chore: fix missing reward text

--- components/sections/communities/challenge/Rewards.vue
+++ components/sections/communities/challenge/Rewards.vue
@@ -1,44 +1,22 @@
 <template>
   <Section :title="$t('communities.overview.reward.title')">
     <div class="grid grid-cols-1 xl:grid-cols-2">
-      <div
-        v-for="(reward, i) in rewards"
-        :key="i"
-        class="flex mt-6 items-center"
-      >
-        <div class="sm:flex space-x-3 items-center">
-          <div class="flex sm:flex-none space-x-5 pb-3 md:w-1/2">
-            <div class="flex-none">
-              <Coin :token="reward.token" size="medium" />
-            </div>
-            <div class="text-base flex-none pr-3">
-              <span class="block font-medium"
-                >{{ reward.amount }} {{ reward.token }}</span
-              >
-              <div class="block text-sm">
-                {{ $t('bounties.rewards.per') }}
-                <span class="capitalize">{{ reward.type.toLowerCase() }}</span>
-              </div>
-            </div>
-          </div>
-          <div class="text-base pl-16 md:pl-0 md:-mt-4 max-w-1/2 md:flex-grow">
-            <span class="block font-normal">{{ reward.description }}</span>
-          </div>
-        </div>
+      <div v-for="(reward, i) in rewards" :key="i">
+        <Reward :reward="reward" />
       </div>
     </div>
   </Section>
 </template>
 <script>
 import { mapGetters } from 'vuex'
-import Coin from '@/components/ui/Coin'
+import Reward from '@/components/ui/Reward'
 import Section from '../partials/Section.vue'
 
 export default {
   name: 'OverviewRewards',
   components: {
     Section,
-    Coin,
+    Reward,
   },
   computed: {
     ...mapGetters({
