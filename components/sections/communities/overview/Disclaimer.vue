4e9a15f82c6007de656fbbd99498bfda8cb20977
musayann <musayann@octangrp.com>
2021-06-17 13:36:38 +0200

    chore: add disclaimer and prerequisite sections

---
+++ components/sections/communities/overview/Disclaimer.vue
@@ -0,0 +1,24 @@
+<template>
+  <Section
+    v-if="community.disclaimer"
+    :title="$t('communities.overview.info.disclaimer.title')"
+    ><span class="block text-lg pt-2">{{ community.disclaimer }}</span>
+  </Section>
+</template>
+<script>
+/* eslint-disable no-console */
+import { mapGetters } from 'vuex'
+import Section from '../partials/Section.vue'
+
+export default {
+  name: 'DisclaimerSection',
+  components: {
+    Section,
+  },
+  computed: {
+    ...mapGetters({
+      community: 'communities/current',
+    }),
+  },
+}
+</script>
---
+++ components/ui/Hint.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="
+      bg-yellow-50
+      text-yellow-900
+      py-5
+      px-8
+      border border-solid border-yellow-100
+      text-base
+      hint-container
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hint',
+}
+</script>
+<style lang="scss">
+.hint-container {
+  a {
+    font-weight: 500;
+  }
+}
+</style>
