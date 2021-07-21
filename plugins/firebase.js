18dd414a02e7486fbe46448a9e30839fa5c838c7
musayann <musayann@octangrp.com>
2021-07-21 20:32:42 +0200

    feat: add firebase analytics events collection

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/analytics'
 
 if (!firebase.apps.length) {
   let config = {}
   config = {
     apiKey: process.env.NUXT_ENV_FIREBASE_API_KEY,
     projectId: process.env.NUXT_ENV_FIREBASE_PROJECT_ID,
+    appId: process.env.NUXT_ENV_FIREBASE_APP_ID,
+    measurementId: process.env.NUXT_ENV_FIREBASE_MEASUREMENT_ID,
   }
   firebase.initializeApp(config)
 }
+
+firebase.analytics()
 
 export default firebase
