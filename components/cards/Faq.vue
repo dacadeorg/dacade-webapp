02135e64ed255a62547d47b3c7e208fb0a9d2e34
Yannick Musafiri <musayann@octangrp.com>
2021-08-24 08:03:47 +0000

    feat: add faq section to the community overview page

---
+++ components/cards/Faq.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="py-5 md:py-10 w-full bg-gray-50">
+    <div
+      class="
+        w-full
+        content-wrapper
+        divide-y divide-2
+        lg:divide-0 lg:divide-y-0
+        divde-gray-100
+        flex flex-col
+        lg:flex-row
+      "
+    >
+      <div
+        class="
+          w-full
+          lg:w-1/2
+          lg:min-w-xs
+          text-gray-900 text-2xl
+          md:text-4xl
+          font-medium
+          pb-5
+          flex-row
+        "
+      >
+        <div>
+          {{ $t('faq.title') }}
+        </div>
+        <div
+          class="
+            hidden
+            text-gray-500 text-base
+            font-normal
+            lg:inline-block
+            mt-2
+            max-w-xs
+          "
+        >
+          {{ $t('faq.subtitle') }}
+          <a
+            class="hover:text-gray-700 underline"
+            href="https://discord.gg/U38KQHDtHe"
+            target="_blank"
+          >
+            {{ $t('faq.discord') }}
+          </a>
+        </div>
+      </div>
+      <div
+        v-if="community.faq"
+        class="w-full lg:w-1/2 divide-y divide-2 space-y-4 divde-gray-100"
+      >
+        <QuestionCard
+          v-for="(question, i) in community.faq"
+          :key="i"
+          :details="question"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import QuestionCard from '@/components/cards/Question'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'FaqCard',
+  components: { QuestionCard },
+  computed: {
+    ...mapGetters({
+      community: 'communities/current',
+    }),
+  },
+}
+</script>
---
+++ components/cards/Question.vue
@@ -0,0 +1,68 @@
+/* eslint-disable vue/no-v-html */
+<template>
+  <div
+    v-click-outside="externalClick"
+    class="text-gray-500 text-base md:text-lg pt-4"
+  >
+    <div
+      :class="show ? 'text-gray-900 font-medium' : ''"
+      class="
+        flex
+        justify-between
+        space-x-5
+        align-middle
+        hover:text-gray-900
+        font-medium
+        cursor-pointer
+      "
+      @click="toggle"
+    >
+      {{ details.title }}
+
+      <ChevronTopIcon v-if="show" class="ml-4 mt-2" />
+      <ChevronBottomIcon v-else class="ml-4 mt-2" />
+    </div>
+    <div
+      v-show="show"
+      class="mt-3 text-gray-700 pr-5 prose"
+      v-html="details.description"
+    />
+  </div>
+</template>
+<script>
+import vClickOutside from 'v-click-outside'
+import ChevronTopIcon from '~/assets/icons/chevron-top.svg?inline'
+import ChevronBottomIcon from '~/assets/icons/chevron-bottom.svg?inline'
+export default {
+  name: 'QuestionCard',
+  directives: {
+    clickOutside: vClickOutside.directive,
+  },
+  components: {
+    ChevronTopIcon,
+    ChevronBottomIcon,
+  },
+  props: {
+    details: {
+      default: () => {},
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      show: false,
+      answer: this.details.a,
+    }
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show
+    },
+    externalClick() {
+      if (this.show) {
+        this.show = false
+      }
+    },
+  },
+}
+</script>
