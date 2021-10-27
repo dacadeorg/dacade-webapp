1b886d1d9dd73545dc80375df1228139b7160051
musayann <musayann@octangrp.com>
2021-10-27 20:02:53 +0200

    fix: token fetching

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,7 +1,12 @@
-import { onAuthStateChanged } from 'firebase/auth'
+import { onAuthStateChanged, onIdTokenChanged } from 'firebase/auth'
 import { auth } from '@/plugins/firebase'
 export default (context) => {
   const { store } = context
+
+  onIdTokenChanged(auth, async (user) => {
+    await store.dispatch('user/getToken')
+  })
+
   return new Promise((resolve, reject) => {
     return onAuthStateChanged(auth, (user) => {
       resolve(store.dispatch('user/fetch', user))
