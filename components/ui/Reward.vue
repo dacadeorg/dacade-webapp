85244768f5257e1347c673ee9cb0bf97b5c587d6
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2021-12-29 16:08:50 +0100

    fix: broken design community courses overview

--- components/ui/Reward.vue
+++ components/ui/Reward.vue
@@ -2,11 +2,24 @@
   <div>
     <div
       :v-if="reward == true && size == 'medium'"
-      class="flex items-center bg-gray-200 rounded-full max-w-max"
+      class="flex items-center rounded-full max-w-max"
     >
       <Coin :token="reward.token" :size="size" />
-      <div class="text-base lg:pl-2 lg:pr-3 md:px-2">
-        <span class="block font-medium text-md">{{ reward.token }}</span>
+      <div class="text-base lg:pl-2 lg:pr-3 md:px-2 max-w-max">
+        <div class="flex">
+          <span class="block font-medium text-md pr-1">{{
+            reward.amount
+          }}</span>
+          <span class="block font-medium text-md">{{ reward.token }}</span>
+        </div>
+        <div class="flex">
+          <div class="text-gray-500 text-base font-normal pr-1">
+            {{ $t('reward.type.prefix') }}
+          </div>
+          <div class="text-gray-500 text-base font-normal">
+            {{ $t('communities.challenge.submission') }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
