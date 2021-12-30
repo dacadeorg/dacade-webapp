7d72494e19f6f00bd271bb39d9b64261cc9b1126
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2021-12-30 10:31:22 +0100

    fix: issues with coin sizes

--- components/sections/communities/courses/overview/Rewards.vue
+++ components/sections/communities/courses/overview/Rewards.vue
@@ -22,6 +22,10 @@ export default {
     Section,
     Reward,
   },
+  size: {
+    required: true,
+    type: String,
+  },
   computed: {
     ...mapGetters({
       course: 'communities/courses/current',
