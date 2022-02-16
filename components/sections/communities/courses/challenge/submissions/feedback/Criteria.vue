f87dbd5e8978226472d80f8d66a3e0a0431dc00e
karemarsy <karemarsy@gmail.com>
2022-02-16 12:32:50 +0200

    chore: make the feedback rewards active

--- components/sections/communities/courses/challenge/submissions/feedback/Criteria.vue
+++ components/sections/communities/courses/challenge/submissions/feedback/Criteria.vue
@@ -2,7 +2,8 @@
   <div
     class="py-5 border rounded-t relative"
     :class="
-      submission.reviewable
+      !submission.reviewable &
+      !(submission.metadata && submission.metadata.evaluation)
         ? 'bg-gray-50 border-gray-200'
         : 'bg-yellow-50 border-yellow-200'
     "
@@ -21,15 +22,35 @@
         h-10
         sm:w-15 sm:h-15
       "
-      :bg-color="submission.reviewable ? '#d2d2d2' : null"
-      :color="submission.reviewable ? '#FFFFFF' : null"
+      :bg-color="
+        !submission.reviewable &
+        !(submission.metadata && submission.metadata.evaluation)
+          ? '#d2d2d2'
+          : null
+      "
+      :color="
+        !submission.reviewable &
+        !(submission.metadata && submission.metadata.evaluation)
+          ? '#FFFFFF'
+          : null
+      "
     />
-    <div :class="submission.reviewable ? 'text-gray-700' : 'text-yellow-900'">
+    <div
+      :class="
+        !submission.reviewable &
+        !(submission.metadata && submission.metadata.evaluation)
+          ? 'text-gray-700'
+          : 'text-yellow-900'
+      "
+    >
       <div class="relative">
         <div
           class="divide-y sm:divide-y-0 space-y-4"
           :class="
-            submission.reviewable ? 'divide-gray-200' : 'divide-yellow-200'
+            !submission.reviewable &
+            !(submission.metadata && submission.metadata.evaluation)
+              ? 'divide-gray-200'
+              : 'divide-yellow-200'
           "
         >
           <div class="sm:pl-10 pl-15">
@@ -59,7 +80,10 @@
               sm:pt-0
             "
             :class="
-              submission.reviewable ? 'divide-gray-200 ' : 'divide-yellow-200'
+              !submission.reviewable &
+              !(submission.metadata && submission.metadata.evaluation)
+                ? 'divide-gray-200 '
+                : 'divide-yellow-200'
             "
           >
             <div
@@ -94,7 +118,10 @@
             v-show="infoVisibility === true"
             class="divide-y space-y-4"
             :class="
-              submission.reviewable ? 'border-gray-200' : 'border-yellow-200'
+              !submission.reviewable &
+              !(submission.metadata && submission.metadata.evaluation)
+                ? 'border-gray-200'
+                : 'border-yellow-200'
             "
           >
             <div
@@ -102,7 +129,10 @@
               :key="key"
               class="pt-6 px-3.75 sm:px-10 pb-0 sm:border-t font-medium"
               :class="
-                submission.reviewable ? 'border-gray-200' : 'border-yellow-200'
+                !submission.reviewable &
+                !(submission.metadata && submission.metadata.evaluation)
+                  ? 'border-gray-200'
+                  : 'border-yellow-200'
               "
             >
               <span class="relative block">{{
@@ -111,11 +141,19 @@
               <div
                 class="sm:-ml-6 px-5 sm:p-0"
                 :class="
-                  submission.reviewable ? 'text-gray-600' : 'text-yellow-900'
+                  !submission.reviewable &
+                  !(submission.metadata && submission.metadata.evaluation)
+                    ? 'text-gray-600'
+                    : 'text-yellow-900'
                 "
               >
                 <ObjectiveList
-                  :iconcolor="submission.reviewable ? '#9CA3AF' : '#F59E0B'"
+                  :iconcolor="
+                    !submission.reviewable &
+                    !(submission.metadata && submission.metadata.evaluation)
+                      ? '#9CA3AF'
+                      : '#F59E0B'
+                  "
                   :crossmark="!item.positive"
                   :objectives="item.criteria"
                 />
