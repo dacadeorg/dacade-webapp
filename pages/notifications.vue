268f7b36c21bfcfd59b2b4e1807e5a19dafd7f06
musayann <musayann@octangrp.com>
2022-03-28 19:04:58 +0200

    chore: fix linting issues

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -107,8 +107,8 @@ export default {
   border: 1.6px solid #53d1af;
   background: #343b42;
   padding: 1em;
-  box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%),
-    0 8px 10px 1px rgb(0 0 0 / 14%), 0 3px 14px 2px rgb(0 0 0 / 12%);
+  box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%), 0 8px 10px 1px rgb(0 0 0 / 14%),
+    0 3px 14px 2px rgb(0 0 0 / 12%);
 }
 
 .notification:hover a {
