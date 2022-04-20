e33fe0ca490b54c4d873c19e8637824c3d82d51e
musayann <musayann@octangrp.com>
2022-04-20 21:14:48 +0200

    chore: hide review deadline on bounty when expired

--- components/cards/Bounty.vue
+++ components/cards/Bounty.vue
@@ -59,7 +59,7 @@
                 }}
               </div>
             </div>
-            <div class="text-gray-500 text-base font-normal">
+            <div v-if="submission.reviewable" class="text-gray-500 text-base font-normal">
               {{ $t('bounties.prefix.closes') }}
               {{ convertDate(submission.reviewDeadline) }}
             </div>
