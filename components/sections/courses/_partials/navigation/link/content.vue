cb8928f10543a0b3d1659b56a86ccf3804852b82
Yannick Musafiri <musayann@octangrp.com>
2022-05-10 09:55:04 +0200

    refactor: divide community navigation into smaller components

---
+++ components/sections/courses/_partials/navigation/link/content.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <span
+      v-show="isActive"
+      class="absolute inline-block top-0 -left-6 nav-icon"
+    >
+      <ChevronRightIcon
+        :class="{
+          'transform rotate-90':
+            item.subitems && item.subitems.length && isActive && expanded,
+        }"
+        class="transition-transform duration-200"
+      />
+    </span>
+    <span class="nav-label">{{ $t(item.label) }}</span>
+  </span>
+</template>
+
+<script>
+import ChevronRightIcon from '~/assets/icons/chevron-right.svg?inline'
+export default {
+  components: {
+    ChevronRightIcon,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    expanded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
---
+++ components/sections/courses/_partials/navigation/link/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <span class="relative block text-sm">
+    <LinkAction
+      :item="item"
+      :is-active="isActive"
+      :active-link-style="activeLinkStyle"
+      @goToLink="goToLink"
+    >
+      <LinkContent :item="item" :is-active="isActive" :expanded="expanded" />
+    </LinkAction>
+    <ul
+      v-if="
+        item.subitems &&
+        item.subitems.length &&
+        isCurrentLink(item.link, false) &&
+        expanded
+      "
+    >
+      <SubLink
+        v-for="(subitem, j) in item.subitems"
+        :key="j"
+        :item="item"
+        :subitem="subitem"
+        :active-link-style="activeLinkStyle"
+      />
+    </ul>
+  </span>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import LinkContent from './content.vue'
+import LinkAction from './action.vue'
+import SubLink from './sub.vue'
+export default {
+  components: {
+    LinkContent,
+    LinkAction,
+    SubLink,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      expanded: true,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      colors: 'ui/colors',
+    }),
+    isActive() {
+      return this.isCurrentLink(this.item.link, this.item.exact)
+    },
+    activeLinkStyle() {
+      return {
+        color: this.colors.primary,
+      }
+    },
+  },
+  methods: {
+    goToLink() {
+      if (this.isCurrentLink(this.item.link, this.item.exact)) {
+        this.expanded = !this.expanded
+        return
+      }
+      this.$router.push(this.localePath(this.item.link))
+    },
+    isCurrentLink(link, exact = false) {
+      if (exact) {
+        return this.$route.path === this.localePath(link)
+      }
+      return this.$route.path.includes(this.localePath(link))
+    },
+  },
+}
+</script>
