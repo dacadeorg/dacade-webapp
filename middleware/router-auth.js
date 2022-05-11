238b5c652625f3c67d49956f35b4f9d8478c9ab9
Yannick Musafiri <musayann@octangrp.com>
2022-05-11 19:53:13 +0200

    fix: verify email page redirect

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -4,8 +4,8 @@
 export default async function ({ store, redirect, route }) {
   const authUser = store.getters['user/data']
   const auth = store.getters['auth/data']
-  console.log(route)
-  if (route.fullPath.startsWith('/verify-email?code=')) {
+
+  if (route.path.startsWith('/verify-email')) {
     return
   }
 
