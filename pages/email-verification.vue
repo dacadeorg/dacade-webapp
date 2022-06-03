26ed30934113a12bdd77f3f8e4227e45061960a5
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2022-06-03 12:05:41 +0200

    fix: eslint issues

--- pages/email-verification.vue
+++ pages/email-verification.vue
@@ -1,28 +1,23 @@
 <template>
-  <div
-    v-if="user"
-    class="md:w-7/12 w-full content-wrapper mx-auto"
-  >
+  <div v-if="user" class="md:w-7/12 w-full content-wrapper mx-auto">
     <div
-      class="relative px-8 border border-solid border-gray-200 py-8 rounded-3.5xl bg-gray-50 "
+      class="relative px-8 border border-solid border-gray-200 py-8 rounded-3.5xl bg-gray-50"
     >
       <div>
         <h1 class="text-3xl font-normal mb-3">
           {{ $t('email-verification.title') }}
         </h1>
-        
-          <p class="text-lg">
-            <span class="font-normal">{{ $t('email-verification.subtitle') }}</span>
-            <span class="font-medium">{{ user.email }}</span>
-            <span class="font-normal" v-html="$t('email-verification.message')"/>
-          </p>
-          <!-- <p class="text-base font-bold mb-4 inline-block"></p>
-          <p
-            class="text-lg py-4 inline-block"
-            
-          /> -->
 
-        <p class="text-sm pt-7 pb-5">{{ $t('email-verification.foot-note') }}</p>
+        <p class="text-lg">
+          <span class="font-normal">{{
+            $t('email-verification.subtitle')
+          }}</span>
+          <span class="font-medium">{{ user.email }}</span>
+          <span class="font-normal" v-html="$t('email-verification.message')" />
+        </p>
+        <p class="text-sm pt-7 pb-5">
+          {{ $t('email-verification.foot-note') }}
+        </p>
       </div>
 
       <div class="mt-1">
