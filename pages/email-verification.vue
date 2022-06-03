9dde814941cb1002bbb36bd24afcbeaea6aa3c80
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2022-06-03 11:58:06 +0200

    feat: Implement email verification design

--- pages/email-verification.vue
+++ pages/email-verification.vue
@@ -1,22 +1,31 @@
 <template>
   <div
     v-if="user"
-    class="flex items-center justify-center absolute min-h-screen top-0 w-full"
+    class="md:w-7/12 w-full content-wrapper mx-auto"
   >
-    <div class="relative p-6 text-center">
+    <div
+      class="relative px-8 border border-solid border-gray-200 py-8 rounded-3.5xl bg-gray-50 "
+    >
       <div>
-        <h1 class="text-3xl font-medium mb-7">
+        <h1 class="text-3xl font-normal mb-3">
           {{ $t('email-verification.title') }}
         </h1>
-        <p class="text-lg">
-          {{ $t('email-verification.subtitle') }}
-        </p>
-        <p class="text-base font-bold mb-4">{{ user.email }}</p>
-        <p class="text-lg py-4" v-html="$t('email-verification.message')" />
-        <p class="text-sm py-4">{{ $t('email-verification.foot-note') }}</p>
+        
+          <p class="text-lg">
+            <span class="font-normal">{{ $t('email-verification.subtitle') }}</span>
+            <span class="font-medium">{{ user.email }}</span>
+            <span class="font-normal" v-html="$t('email-verification.message')"/>
+          </p>
+          <!-- <p class="text-base font-bold mb-4 inline-block"></p>
+          <p
+            class="text-lg py-4 inline-block"
+            
+          /> -->
+
+        <p class="text-sm pt-7 pb-5">{{ $t('email-verification.foot-note') }}</p>
       </div>
 
-      <div class="text-center mt-1">
+      <div class="mt-1">
         <ArrowButton :loading="loading" @click="resendEmail()">
           {{ $t('email-verification.buttons.resend') }}
         </ArrowButton>
