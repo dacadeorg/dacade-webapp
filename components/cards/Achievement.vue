551521c585a91203b23fa443ff6be807f06aac87
musayann <musayann@octangrp.com>
2022-09-09 12:47:02 +0300

    fix: date localization

--- components/cards/Achievement.vue
+++ components/cards/Achievement.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="border border-solid rounded-3.5xl px-7 py-9 w-full">
-    <nuxt-link :to="`/profile/${username}/achievements/${data.id}`">
+    <nuxt-link :to="localePath(`/profile/${username}/achievements/${data.id}`)">
       <div class="mx-auto w-full text-left">
         <div
           class="w-20 h-20 p-5 mx-auto rounded-full mb-5"
