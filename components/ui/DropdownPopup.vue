bb406ddc423ce6ca5aec1ee9e0fff3d2e64de346
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2022-10-04 10:53:23 +0200

    fix: the padding issue

--- components/ui/DropdownPopup.vue
+++ components/ui/DropdownPopup.vue
@@ -5,7 +5,6 @@
       maxWidth: '340px',
       maxHeight: 'calc(100vh - 100px)',
       overflow: 'hidden scroll',
-      padding: '16px',
     }"
     class="absolute top-14 right-0 z-40 bg-white rounded-3.5xl no-scrollbar text-gray-900"
   >
