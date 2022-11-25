545e95e09b21f2a4d354b10f00d82cde8ef16a86
musayann <musayann@gmail.com>
2022-11-25 12:59:35 +0200

    fix: aeternity address truncating

--- utilities/Address.js
+++ utilities/Address.js
@@ -1,7 +1,7 @@
 const aeAllowedChars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
 const ethRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
-const aeRegex = `^(ak_[${aeAllowedChars}]{5})+([${aeAllowedChars}]{37})+([${aeAllowedChars}]{8})$`;
+const aeRegex = `^(ak_)([${aeAllowedChars}]+)$`;
 const nearRegex = /^([a-fA-F0-9]{5})+([a-fA-F0-9]{49})+([a-fA-F0-9]{10})$/;
 const algoRegex = /^([A-Z2-7]{6})+([A-Z2-7]{46})+([A-Z2-7]{6})$/;
 
@@ -15,7 +15,7 @@ export const truncateEthAddress = (address) => {
 };
 
 export const truncateAEAddress = (address) => {
-  return truncateHandler(address, aeRegex, (match) => `${match[1]}…${match[3]}`);
+  return truncateHandler(address, aeRegex, () => `${address.slice(0, 8)}…${address.slice(address.length - 8, address.length)}`);
 };
 
 export const truncateNearAddress = (address) => {
@@ -26,8 +26,10 @@ export const truncateAlgoAddress = (address) => {
   return truncateHandler(address, algoRegex, (match) => `${match[1]}…${match[3]}`);
 };
 
-export const truncateAddress = (address, token = "eth")=> {
-  switch (token.toLowerCase()){
+export const truncateAddress = (rawAddress, token = "eth") => {
+  const address = rawAddress.trim();
+  
+  switch (token.trim().toLowerCase()) {
     case 'near':
       return truncateNearAddress(address);
     case 'ae':
