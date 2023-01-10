a55b4aa39e3781fa001c27ccdf59547f4cb62274
emmanuel <Emmanuelsumeh@gmail.com>
2023-01-10 04:21:14 +0100

    ft: implement new discord verification workflow designs

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -11,6 +11,33 @@
     <ProfileOverviewSection title="Notifications" see-more>
       <NotificationList extended />
     </ProfileOverviewSection>
+
+    <!--     connecting to discord modal -->
+    <Modal :show="showDiscordModal" size="medium">
+      <div class="px-6 pt-6">
+        <div class="pb-7">
+          <p class="text-.5xl font-medium leading-snug">
+            {{ $t('profile.header.discord') }}
+          </p>
+        </div>
+
+        <div class="pb-7">
+          <p :class="discordError ? 'bg-red-50 text-red-700' : discordSuccess ? 'bg-green-50 text-green-700' : 'bg-white'"
+             class="p-3 rounded text-.2xl font-medium leading-snug">
+            {{ getDiscordMessage() }}
+          </p>
+        </div>
+
+        <div class="pb-7">
+          <Button :class="discordLoading && 'text-gray-500'" :disabled="discordLoading"
+                  class="bg-white disabled:text-gray-500 -ml-4 text-blue-500 font-semibold py-2 px-4 border-none! rounded"
+                  @click="closeModal()">
+            {{ $t('profile.header.discord.close') }}
+          </Button>
+        </div>
+      </div>
+
+    </Modal>
   </div>
 </template>
 
@@ -21,11 +48,13 @@ import ProfileOverviewCommunities from '~/components/sections/profile/overview/C
 import ProfileOverviewAchievements from '~/components/sections/profile/overview/Achievements'
 import ProfileOverviewReferrals from '~/components/sections/profile/overview/Referrals'
 import ProfileOverviewSection from '~/components/sections/profile/overview/Section'
-import { getMetadataTitle } from '~/utilities/Metadata'
+import {getMetadataTitle} from '~/utilities/Metadata'
+import Modal from "~/components/ui/Modal.vue";
 
 export default {
   name: 'ProfileOverview',
   components: {
+    Modal,
     ProfileOverviewSection,
     ProfileOverviewReferrals,
     ProfileOverviewAchievements,
@@ -37,9 +66,62 @@ export default {
   data() {
     return {
       username: '',
+      discordError: false,
+      discordSuccess: false,
+      discordLoading: false,
+      showDiscordModal: false,
     }
   },
-  fetch({ store, params, error }) {
+  mounted() {
+    //   call discord api with token param
+    this.discordCallback()
+  },
+  methods: {
+    async discordCallback() {
+      try {
+        const code = this.$route.query.code
+        if (!code) {
+          return
+        }
+
+        this.discordLoading = true
+        this.showDiscordModal = true
+
+        const res = await this.$api.post('auth/discord', {
+          code,
+        })
+        if (!res) {
+          this.discordError = true
+          return
+        }
+        this.discordSuccess = true
+      } catch (e) {
+        console.log({e})
+        this.discordError = true
+      } finally {
+        this.discordLoading = false
+      }
+
+    },
+
+    closeModal() {
+      this.showDiscordModal = false
+      this.discordError = false
+      this.discordSuccess = false
+      this.discordLoading = false
+    },
+
+    getDiscordMessage() {
+      if (this.discordError) {
+        return this.$t('profile.header.discord.error')
+      }
+      if (this.discordSuccess) {
+        return this.$t('profile.header.discord.success')
+      }
+      return this.$t('profile.header.discord.connect')
+    }
+  },
+  fetch({store, params, error}) {
     this.username = store.getters['auth/get'].displayName
 
     return Promise.all([
