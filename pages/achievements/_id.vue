cfc0977020835310a51b862b49b1e09b111c29d9
musayann <musayann@gmail.com>
2023-03-30 15:10:56 +0200

    feat: add preview link

--- pages/achievements/_id.vue
+++ pages/achievements/_id.vue
@@ -9,8 +9,13 @@
           class="flex justify-center items-center p-7 md:rounded-l-3xl rounded-t-3xl md:rounded-t-none w-full md:w-1/2 md:bg-none bg-gray-100"
         >
           <div
-            :class="['h-52 w-52', { 'p-12 rounded-full': isNotCertificateIcon }]"
-            :style="{ backgroundColor: isNotCertificateIcon && backgroundColor }"
+            :class="[
+              'h-52 w-52',
+              { 'p-12 rounded-full': isNotCertificateIcon },
+            ]"
+            :style="{
+              backgroundColor: isNotCertificateIcon && backgroundColor,
+            }"
           >
             <img :src="achievement?.metadata?.image" alt="certificate badge" />
           </div>
@@ -23,7 +28,10 @@
               {{ achievement?.metadata?.name }}
             </h2>
             <p class="text-gray-700 md:text-base text-sm">
-              {{ achievement?.metadata?.narrative || achievement?.metadata?.description }}
+              {{
+                achievement?.metadata?.narrative ||
+                achievement?.metadata?.description
+              }}
             </p>
           </div>
 
@@ -63,23 +71,34 @@
             >
               <AchievementLinkField :link="achievement?.metadata?.linkToWork" />
             </AchievementViewItem>
-            <div v-if="mintable" class="w-full flex">
-              <MintCertificate
-                v-if="!achievementMinted && belongsToCurrentUser"
-                :show="showMintCertificate"
-                @close="showMintCertificate = false"
-              />
-              <ArrowButton
-                v-if="belongsToCurrentUser && !minted"
-                target="__blank"
-                type="primary"
-                class="flex ml-auto mt-5"
-                @click="showMintCertificate = true"
-              >
-                Mint certificate
-              </ArrowButton>
+            <div v-if="mintable">
+              <AchievementViewItem v-if="belongsToCurrentUser && !minted" :name="$t('profile.achievement.ipfs-metadata')">
+                <a
+                  :href="metadataPreviewURL"
+                  target="_blank"
+                  class="text-xs underline"
+                  >Preview</a
+                >
+              </AchievementViewItem>
+              <div class="w-full flex">
+                <MintCertificate
+                  v-if="!achievementMinted && belongsToCurrentUser"
+                  :show="showMintCertificate"
+                  @close="showMintCertificate = false"
+                />
+                <ArrowButton
+                  v-if="belongsToCurrentUser && !minted"
+                  target="__blank"
+                  type="primary"
+                  class="flex ml-auto mt-5"
+                  @click="showMintCertificate = true"
+                >
+                  Mint certificate
+                </ArrowButton>
+              </div>
             </div>
           </div>
+
           <div
             v-if="minted"
             class="pt-5 mt-5 flex flex-col md:gap-3 gap-3 border-t border-t-solid"
@@ -217,6 +236,9 @@ export default {
     },
     contractURL() {
       return `${process.env.NUXT_ENV_BLOCK_EXPLORER_URL}/address/${this.achievement?.minting?.contract}`
+    },
+    metadataPreviewURL() {
+      return `${process.env.NUXT_ENV_API_BASE_URL}/certificates/${this.achievement?.id}/metadata-preview`
     },
     belongsToCurrentUser() {
       if (!this.user) return false
