b8e6fa1eded96a7f446769356c7c46b2fb20bf08
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2023-05-10 10:46:57 +0200

    amends typos

--- components/sections/profile/settings/Overview.vue
+++ components/sections/profile/settings/Overview.vue
@@ -9,9 +9,6 @@
         class="grid grid-cols-3 gap-4 p-4">
         <div class="text-gray text-sm">{{ $t('profile.edit.username') }}</div>
         <div class="text-gray text-sm">{{ user.displayName }}</div>
-        <button
-          class="bg-transparent hover:bg-transparent flex justify-end text-primary text-xs"
-            @click="console.log()"></button>
       </div>
 
       <div
@@ -20,7 +17,7 @@
         <div class="text-gray text-sm">{{ user.email }}</div>
         <button
           class="bg-transparent hover:bg-transparent flex justify-end text-primary text-xs"
-          @click=" togglePopupEmail()"
+          @click="togglePopupEmail()"
         >{{ user?.email?.length ? 'Change' : 'Set' }}
         </button>
       </div>
@@ -31,7 +28,7 @@
         <div class="text-gray text-sm">{{ fullName }}</div>
         <button
           class="bg-transparent hover:bg-transparent flex justify-end text-primary text-xs"
-          @click=" togglePopupNames()"
+          @click="togglePopupNames()"
         >{{ fullName.length ? 'Change' : 'Set' }}
         </button>
       </div>
@@ -42,7 +39,7 @@
         :show-popup="showPopup"
         class="w-3/5"
         @close="togglePopUp()"
-        @togglePopup="() => togglePopUp()"/>
+        @togglePopup="togglePopUp()"/>
 
     </ProfileSettingsSection>
   </template>
