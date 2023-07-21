467d59d104f54620b9fbdbd4ffb7b9b030b02e10
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-07-21 18:29:26 +0300

    Update Address.js
    Added icp regex and icp address validation

--- utilities/Address.js
+++ utilities/Address.js
@@ -5,6 +5,7 @@ const ethRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
 const aeRegex = `^(ak_)([${aeAllowedChars}]+)$`
 const nearRegex = /^([a-fA-F0-9]{5})+([a-fA-F0-9]{49})+([a-fA-F0-9]{10})$/
 const algoRegex = /^([A-Z2-7]{6})+([A-Z2-7]{46})+([A-Z2-7]{6})$/
+const icpRegex = /^([a-zA-Z0-9]{4})+([a-zA-Z0-9]{46})+([a-zA-Z0-9]{4})$/
 
 const validateRegex = (address, regex) => {
   const match = address.match(regex)
@@ -52,40 +53,55 @@ export const truncateAlgoAddress = (address) => {
   )
 }
 
+export const truncateIcpAddress = (address) => {
+  return truncateHandler(
+    address,
+    icpRegex,
+    (match) => `${match[1]}…${match[3]}`
+  )
+}
+
 export const truncateAddress = (rawAddress, token = 'eth') => {
-  if (!rawAddress) return
+  if (!rawAddress) return;
 
-  const address = rawAddress.trim()
+  const address = rawAddress.trim();
+  const tokenLowerCase = token.trim().toLowerCase();
 
-  switch (token.trim().toLowerCase()) {
+  switch (tokenLowerCase) {
     case 'near':
-      return truncateNearAddress(address)
+      return truncateNearAddress(address);
     case 'ae':
-      return truncateAEAddress(address)
+      return truncateAEAddress(address);
     case 'algo':
-      return truncateAlgoAddress(address)
+      return truncateAlgoAddress(address);
+    case 'icp': // Added ICP case here
+      return truncateIcpAddress(address);
     default:
-      return truncateEthAddress(address)
+      return truncateEthAddress(address);
   }
 }
 
 export const validateAddress = (address, token) => {
-  if (!address) return false
+  if (!address) return false;
 
-  const trimmedAddress = address.trim()
-  const tokenLowerCase = token.toLowerCase()
+  const trimmedAddress = address.trim();
+  const tokenLowerCase = token.toLowerCase();
 
   if (tokenLowerCase === 'near') {
-    return validateRegex(trimmedAddress, nearRegex)
+    return validateRegex(trimmedAddress, nearRegex);
   }
 
   if (tokenLowerCase === 'ae') {
-    return validateRegex(trimmedAddress, aeRegex)
+    return validateRegex(trimmedAddress, aeRegex);
   }
 
   if (['algo', 'usdc'].includes(tokenLowerCase)) {
-    return validateRegex(trimmedAddress, algoRegex)
+    return validateRegex(trimmedAddress, algoRegex);
   }
 
-  return validateRegex(trimmedAddress, ethRegex)
+  if (tokenLowerCase === 'icp') { // Added ICP here
+    return validateRegex(trimmedAddress, icpRegex);
+  }
+
+  return validateRegex(trimmedAddress, ethRegex);
 }
